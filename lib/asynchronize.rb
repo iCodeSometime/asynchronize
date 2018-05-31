d8e34dea6dc736480cbc701915d39b0315746471
Kenneth Cochran <kenneth.cochran101@gmail.com>
2018-05-31 16:23:00 -0500

    added documentation, and fixed bug where it wouldn't properly see old  method_missing

--- lib/asynchronize.rb
+++ lib/asynchronize.rb
@@ -11,6 +11,17 @@ module Asynchronize
       # threads adding methods to your class.
       @@methods_asyncing = Set.new
 
+      ##
+      # Call to asynchronize a method.
+      #   That method will be added to a list of methods to asynchronize.
+      #   If the method already exists, it will be redefined to an asynchronous
+      #   version. If it does not, method_missing will redefine it when it does.
+      #   If the method is redefined afterwards, method_missing will also
+      #   asynchronize that version.
+      #
+      # @param methods [Symbol] The methods to be asynchronized.
+      # @example To add any number of methods to be asynchronized.
+      #   asynchronize :method1, :method2, :methodn
       def self.asynchronize(*methods)
         @@methods_to_async.merge(methods)
         methods.each do |method|
@@ -20,22 +31,33 @@ module Asynchronize
         end
       end
 
+      # require 'pry'; binding.pry
       # Save the old method_added so we don't overwrite it.
-      if method_defined? :method_added
-        alias_method :old_method_added, :method_added
-        undef_method(:method_added)
+      if self.methods.include? :method_added
+        singleton_class.send(:alias_method, :old_method_added, :method_added)
+        singleton_class.undef_method(:method_added)
       end
 
+      ##
+      # Will asynchronize a method if it has not been asynchronized already, and
+      #   it is in the list of methods to asynchronize. If method missing was
+      #   already defined, it will call the previous method_missing before
+      #   anything else Ruby calls this automatically when defining a method; it
+      #   should not be called directly.
       def self.method_added(method)
         # Don't do anything else if we're not actually adding a new method
         return if @@methods_asyncing.include? method
-        old_method_added(method) if method_defined? :old_method_added
+        @@methods_asyncing.add(method)
+        self.old_method_added(method) if self.methods.include? :old_method_added
         return unless @@methods_to_async.include? method
+        # This will delete from @@methods_asyncing
         Asynchronize.create_new_method(method, self)
       end
     end
   end
 
+  ##
+  # Responsible for actually creating the new methods and removing the old.
   def self.create_new_method(method, klass)
     klass.instance_eval do
       old_method = instance_method(method)
